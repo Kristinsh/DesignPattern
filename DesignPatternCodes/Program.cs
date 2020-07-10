f7fb986db095f292b9644ceb3f66295aca3d3a78
Kristin Hagen <kristin.haa@hotmail.com>
2020-07-10 11:45:40 +0200

    CommitFirst

---
+++ DesignPatternCodes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatternCodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
